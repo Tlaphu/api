88f18aaac561b88162f1a2727501a7abc8584b0b
phu <phuthgce182003@fpt.edu.vn>
2025-11-19 01:35:49 +0700

    fix

--- src/main/java/com/ra/base_spring_boot/services/impl/CompanyAuthServiceImpl.java
+++ src/main/java/com/ra/base_spring_boot/services/impl/CompanyAuthServiceImpl.java
@@ -189,7 +189,7 @@ public class CompanyAuthServiceImpl implements ICompanyAuthService {
 
         String frontendBaseUrl = "http://localhost:5173";
 
-        String resetLink = frontendBaseUrl + "/reset-password?token=" + resetToken;
+        String resetLink = frontendBaseUrl + "/reset-password?token=" + resetToken + "&role=COMPANY";
 
         emailService.sendResetPasswordEmail(
                 accountCompany.getEmail(),
